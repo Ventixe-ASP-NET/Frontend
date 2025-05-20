d20e3a5070273d6d91068cdb787b8cadd523e88d
LudThe <46929724+LudThe@users.noreply.github.com>
2025-05-20 14:58:00 +0200

    added stats and events

--- WebApp/Controllers/DashboardController.cs
+++ WebApp/Controllers/DashboardController.cs
@@ -1,13 +1,42 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.Views.Bookings.BookingModels;
 
 namespace WebApp.Controllers;
 
 [Authorize(Roles = "Admin")]
 public class DashboardController : Controller
 {
-    public IActionResult Index()
+
+    private readonly HttpClient _bookingClient;
+    private readonly HttpClient _eventClient;
+
+    public DashboardController(IHttpClientFactory factory)
     {
-        return View();
+        _eventClient = factory.CreateClient("eventApi");
+        _bookingClient = factory.CreateClient("bookingApi");
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var bookingStats = await _bookingClient.GetFromJsonAsync<BookingStatsModel>("api/bookings/stats");
+        var events = await _eventClient.GetFromJsonAsync<EventListWrapper>("api/event");
+
+        List<EventDto> firstEvents = [];
+
+        if (events != null)
+        {
+            firstEvents = events.Events.Take(3).ToList();
+        }
+
+        var viewModel = new DashboardViewModel
+        {
+            BookingStats = bookingStats,
+            UpcomingEvents = 0,
+            Events = firstEvents
+        };
+
+        return View(viewModel);
     }
 }
---
+++ WebApp/Models/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebApp.Views.Bookings.BookingModels;
+
+namespace WebApp.Models;
+
+public class DashboardViewModel
+{
+    public BookingStatsModel? BookingStats { get; set; } = new();
+    public int UpcomingEvents { get; set; } = 0;
+    public List<EventDto> Events { get; set; } = new();
+}
