19cb7fcba8afdd2bc3783ad8332c0f16c75e1514
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-05-27 13:21:15 +0200

    Fix: InvoiceController (CODES)

--- WebApp/Controllers/InvoiceController.cs
+++ WebApp/Controllers/InvoiceController.cs
@@ -13,15 +13,41 @@ namespace WebApp.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var invoices = await _invoiceClient.GetFromJsonAsync<List<ShowInvoiceViewModel>>("api/invoices");
+            var invoices = await _invoiceClient.GetFromJsonAsync<List<ShowInvoiceViewModel>>("api/invoices?code=DRMrLNX-3fbx6GeIP9o3vN_Pa1g21n4gJyNHUfcqjFERAzFu1SZVGw==");
             return View("Invoices/Index", invoices);
         }
 
         [HttpPost]
         public async Task<IActionResult> UpdateInvoice(string id)
         {
-            var invoice = await _invoiceClient.GetFromJsonAsync<ShowInvoiceViewModel>("api/update");
+            var invoice = await _invoiceClient.GetFromJsonAsync<ShowInvoiceViewModel>("api/update?code=WXgN_-IZC21f1ntwaHtUN6QX6-vuTrwTyRUhc6ycIfbuAzFuwsLiTw==");
             return RedirectToAction("Index", "Invoice");
         }
     }
 }
+
+/*
+ Functions:
+
+CreateInvoice: [POST]
+https://invoiceservice-gtg3ajc5htdcgpgs.swedencentral-01.azurewebsites.net/api/invoices?code=wset0lxLDluzSYBFqkaPJiytI0TeWV6glv1yuRfSmzudAzFuPAb_iA==
+
+DeleteInvoice: [DELETE]
+https://invoiceservice-gtg3ajc5htdcgpgs.swedencentral-01.azurewebsites.net/api/invoices/{id}?code=tnP9IZE0KG0Gi6oNMyIJMcFN545wEvDtfMeX0-Xa0BiMAzFuviJ2Bg==
+
+GetAllInvoices: [GET]
+https://invoiceservice-gtg3ajc5htdcgpgs.swedencentral-01.azurewebsites.net/api/invoices?code=DRMrLNX-3fbx6GeIP9o3vN_Pa1g21n4gJyNHUfcqjFERAzFu1SZVGw==
+
+GetAllStatuses: [GET]
+https://invoiceservice-gtg3ajc5htdcgpgs.swedencentral-01.azurewebsites.net/api/statuses?code=lCoZvuxLiDzLZZmWrAx_QcNnhW9pJUFmgKVacyBn9JbjAzFuuYdxWQ==
+
+GetInvoiceById: [GET]
+https://invoiceservice-gtg3ajc5htdcgpgs.swedencentral-01.azurewebsites.net/api/invoices/{id}?code=tGq5_bnSYXpoU7clqiGxuPjy8hFe7IlHnKMqtQjaZ7snAzFuLzYjgQ==
+
+UpdateInvoice: [PUT]
+https://invoiceservice-gtg3ajc5htdcgpgs.swedencentral-01.azurewebsites.net/api/invoices?code=WXgN_-IZC21f1ntwaHtUN6QX6-vuTrwTyRUhc6ycIfbuAzFuwsLiTw==
+
+
+INVOICE ID:
+ef875fd7-ac84-4552-9493-5e8d49e961e3
+ */
